3caa3fb0557ad431432ba380b9713bc255d2fede
RomaArtur <arturromafurtado@gmail.com>
2025-12-01 22:49:33 -0300

    Replicando código

--- inception.php
+++ inception.php
@@ -1,18 +1,10 @@
 <?php
 
-echo "Bem-vindo(a) ao screen match!\n";
+$nomeFilme = "A Origem";
+$anoLancamento = $argv[1] ?? 2010;
 
-$nomeFilme = "Top Gun - Maverick";
-$anoLancamento = $argv[1] ?? 2022;
-
-$somaDeNotas = 9;
-$somaDeNotas += 6;
-$somaDeNotas += 8;
-$somaDeNotas += 7.5;
-$somaDeNotas += 5;
-
-$notaFilme = $somaDeNotas / 5;
-$planoPrime = true;
+$notaFilme = 10;
+$planoPrime = false;
 
 $incluidoNoPlano = $planoPrime || $anoLancamento < 2020; 
 
@@ -20,9 +12,4 @@ $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
  echo "Nome do filme: $nomeFilme\n"; 
  echo "Nota do filme: $notaFilme\n";
  echo "Ano de lançamento: $anoLancamento\n";
-
-
- 
-
-
 
