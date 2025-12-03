5b6d599b6043a6f931238d9c6ba72c088517eae3
RomaArtur <arturromafurtado@gmail.com>
2025-12-03 00:21:37 -0300

    Match Expression

--- screen-match.php
+++ screen-match.php
@@ -21,8 +21,23 @@ $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
  echo "Nota do filme: $notaFilme\n";
  echo "Ano de lançamento: $anoLancamento\n";
 
+ if ($anoLancamento > 2022) {
+    echo "Esse filme é um lançamento.\n";
+ } elseif ($anoLancamento > 2020 && $anoLancamento <=2022) {
+    echo "Esse filme ainda é novo."; 
+ } else {
+    echo "Esse filme não é um lançamento\n";
+ }
 
- 
+ // Match funciona de forma semelhante ao switch, mas com algumas diferenças. 
+ // No match, não é necessário usar break
+ // Além disso, o match é uma expressão que retorna um valor, enquanto o switch é uma instrução.
 
+$genero = match ($nomeFilme) {
+    "Top Gun - Maverick" => "ação",
+    "Thor: Ragnarok" => "super-heroi",
+    "Se beber não case" => "comedia",
+    default => "gênero desconhecido", 
+};
 
-
+echo "O gênero do filme é: $genero\n";
